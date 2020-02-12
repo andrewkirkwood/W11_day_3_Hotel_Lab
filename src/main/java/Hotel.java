3f3cb10a62dcb6236c6269fa00115cb39599a898
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-02-12 11:48:05 +0000

    hotel class made and tests to check bedrooms and conferencerooms start empty and can add to the list___passed tests

---
+++ src/main/java/Hotel.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Hotel {
+
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+
+    public Hotel(){
+        this.bedrooms = new ArrayList<Bedroom>();
+        this.conferenceRooms = new ArrayList<ConferenceRoom>();
+    }
+
+    public int getTotalBedrooms() {
+        return this.bedrooms.size();
+    }
+
+    public int getTotalConferenceRooms() {
+        return this.conferenceRooms.size();
+    }
+
+    public void addBedroom(Bedroom bedroom){
+        this.bedrooms.add(bedroom);
+    }
+
+    public void addConferenceRoom(ConferenceRoom conferenceRoom) {
+        this.conferenceRooms.add(conferenceRoom);
+    }
+}
---
+++ src/test/java/HotelTest.java
@@ -0,0 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HotelTest {
+    private Hotel hotel1;
+    private Bedroom bedroom1;
+    private ConferenceRoom conferenceRoom1;
+
+    @Before
+    public void before(){
+        hotel1 = new Hotel();
+        bedroom1 = new Bedroom(2, 2, "double", 20);
+        conferenceRoom1 = new ConferenceRoom(10, "Castle View", 100);
+    }
+
+    @Test
+    public void check_bedrooms_list_starts_empty(){
+        assertEquals(0, hotel1.getTotalBedrooms());
+    }
+
+    @Test
+    public void check_conference_rooms_list_starts_empty(){
+        assertEquals(0, hotel1.getTotalConferenceRooms());
+    }
+
+    @Test
+    public void can_add_bedroom_to_bedrooms_list(){
+        hotel1.addBedroom(bedroom1);
+        assertEquals(1, hotel1.getTotalBedrooms());
+    }
+
+    @Test
+    public void can_add_conference_room_to_conference_rooms(){
+        hotel1.addConferenceRoom(conferenceRoom1);
+        assertEquals(1, hotel1.getTotalConferenceRooms());
+    }
+}
+
